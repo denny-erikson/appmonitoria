b7db124c591b17a9ca2a616f6636daeaf2214afb
denny-erikson <denny.erikson@gmail.com>
2024-10-28 23:04:37 -0300

    adjustments test fields

--- monitoria/models.py
+++ monitoria/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from users.models import CustomUser
+from users.models import CustomUser, Profile
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 
@@ -32,24 +32,51 @@ class Address(models.Model):
 
 class BankAccount(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
-    account_number = models.CharField(max_length=20)
-    bank_name = models.CharField(max_length=100)
+    bank_name = models.CharField(max_length=100, null=True, blank=True)
+    account_agency = models.CharField(max_length=20, null=True, blank=True)
+    account_number = models.CharField(max_length=20, null=True, blank=True)
+    key_pix = models.CharField(max_length=100, null=True, blank=True)
+
     def __str__(self):
         return self.bank_name
 
+
+WORK_REGIME = [
+    ('regime_clt', 'Regime CLT'),
+    ('regime_pj', 'Regime PJ'),
+]
+
 class Document(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
-    Documents_type = models.CharField(max_length=50)
-    Documents_number = models.CharField(max_length=20)
+    rg_number = models.CharField(max_length=10, null=True, blank=True)
+    cpf_number = models.CharField(max_length=11, null=True, blank=True)
+    pis_number = models.CharField(max_length=20, null=True, blank=True)
+    cnpj_number = models.CharField(max_length=14, null=True, blank=True)
+    municipal_registration = models.CharField(max_length=50, null=True, blank=True)
+    work_regime = models.CharField(choices=WORK_REGIME, default='regime_clt', max_length=10)
+
     def __str__(self):
         return self.Documents_type
+    
+
+SIZE_CHOICES = [
+    ('P', 'Pequeno'),
+    ('M', 'Médio'),
+    ('G', 'Grande'),
+    ('GG', 'Extra Grande'),
+]
 
 class Uniform(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
-    uniform_type = models.CharField(max_length=50)
-    size = models.CharField(max_length=10)
+    t_shirt_size = models.CharField(max_length=2, blank=True, null=True, help_text="Select t-shirt size", choices=SIZE_CHOICES)
+    pants_size = models.CharField(max_length=2, blank=True, null=True, help_text="Select pants size", choices=SIZE_CHOICES)
+    shorts_size = models.CharField(max_length=2, blank=True, null=True, help_text="Select shorts size", choices=SIZE_CHOICES)
+    jacket_size = models.CharField(max_length=2, blank=True, null=True, help_text="Select jacket size", choices=SIZE_CHOICES)
+    festival_shirt_size = models.CharField(max_length=2, blank=True, null=True, help_text="Select festival shirt size", choices=SIZE_CHOICES)
+    party_uniform_size = models.CharField(max_length=2, blank=True, null=True, help_text="Select party uniform size", choices=SIZE_CHOICES)
+
     def __str__(self):
-        return self.uniform_type
+        return f"Uniforms for {self.user.username}"
 
 class Product(models.Model):
     name = models.CharField(max_length=50)
@@ -58,15 +85,20 @@ class Product(models.Model):
         return self.name
 
 class Event(models.Model):
-    name = models.CharField(max_length=50)
-    teams = models.ManyToManyField("Team", related_name="events")
+    produto = models.ForeignKey('Product', on_delete=models.CASCADE, blank=True, null=True)
+    resort = models.ForeignKey('Resort', on_delete=models.CASCADE, blank=True, null=True)
+    name = models.CharField(max_length=50,  blank=True, null=True)
+    start_date = models.DateField(auto_now_add=False,  blank=True, null=True)
+    end_date = models.DateField(auto_now_add=False,  blank=True, null=True)
+    daily = models.DecimalField(max_digits=2, decimal_places=2,  blank=True, null=True)
+
     def __str__(self):
         return self.name
 
 class Team(models.Model):
-    name = models.CharField(max_length=50)
-    resort = models.OneToOneField("Resort", on_delete=models.CASCADE)
-    payments = models.ManyToManyField("Payment", related_name="teams")
+    event = models.OneToOneField("Event", on_delete=models.CASCADE, blank=True, null=True,)
+    name = models.CharField(max_length=50, blank=True, null=True,)
+    status = models.BooleanField(default=False, blank=True, null=True,)
 
 class Resort(models.Model):
     name = models.CharField(max_length=100)
@@ -86,7 +118,9 @@ class Cancellation(models.Model):
         return self.reason
 
 class Payment(models.Model):
+    event = models.ForeignKey("Event", on_delete=models.CASCADE, null=True, blank=True)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    date = models.DateField()
+
     def __decimal__(self):
         return self.amount
