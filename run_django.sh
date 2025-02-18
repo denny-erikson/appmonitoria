3e1a3e3cf2b56288373b9113bbe3047adc551988
denny-erikson <denny.erikson@gmail.com>
2025-02-18 16:36:34 -0300

    add virtual environment setup in run_django.sh

--- run_django.sh
+++ run_django.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+python3 -m venv venv
 
 # Ativar o ambiente virtual
 source venv/bin/activate  # Para Linux/Mac
@@ -9,6 +10,9 @@ pip install -r requirements.txt
 
 python manage.py makemigrations
 python manage.py migrate
+
+python manage.py createsuperuser
+
 
 # Rodar a aplicação Django
 python manage.py runserver
