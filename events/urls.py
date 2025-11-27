5def084084e7655e6698c5cd5525ef895b480e79
Denny Erikson <denny.erikson@gmail.com>
2025-11-27 12:45:56 -0300

    feat: implement event management views and templates with detail, list, and creation functionalities

--- events/urls.py
+++ events/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import EventWizard
+from .views import EventCreateView, EventDetailView, EventListView, EventWizard
+
+app_name = "events"
 
 urlpatterns = [
+    path("", EventListView.as_view(), name="event_list"),
+    path("new/", EventCreateView.as_view(), name="event_create"),
+    path("<int:pk>/", EventDetailView.as_view(), name="event_detail"),
     path('event_wizard/', EventWizard.as_view(), name='event_wizard'),
 ]
--- events/views.py
+++ events/views.py
@@ -1,9 +1,18 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views import View
+from django.views.generic import DetailView, ListView
 
 from events.models import Availability, Cancellation, Event, Product, Resort, Team
-from events.serializers import AvailabilitySerializer, CancellationSerializer, EventSerializer, ProductSerializer, ResortSerializer, TeamSerializer
+from events.serializers import (
+    AvailabilitySerializer,
+    CancellationSerializer,
+    EventSerializer,
+    ProductSerializer,
+    ResortSerializer,
+    TeamSerializer,
+)
 
 
 from formtools.wizard.views import SessionWizardView
@@ -33,6 +42,76 @@ class ProductViewSet(viewsets.ModelViewSet):
 class ResortViewSet(viewsets.ModelViewSet):
     queryset = Resort.objects.all()
     serializer_class = ResortSerializer
+
+
+class EventListView(ListView):
+    template_name = "events/event_list.html"
+    context_object_name = "events"
+
+    def get_queryset(self):
+        return (
+            Event.objects.select_related("resort")
+            .prefetch_related("team__availabilities__profile")
+            .order_by("start_date", "name")
+        )
+
+
+class EventDetailView(DetailView):
+    template_name = "events/event_detail.html"
+    context_object_name = "event"
+    model = Event
+
+    def get_queryset(self):
+        return Event.objects.select_related("resort").prefetch_related(
+            "team__availabilities__profile__user"
+        )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        team = getattr(self.object, "team", None)
+        context["team"] = team
+        if team:
+            context["availabilities"] = team.availabilities.select_related(
+                "profile", "profile__user"
+            )
+        else:
+            context["availabilities"] = []
+        return context
+
+
+class EventCreateView(View):
+    template_name = "events/event_form.html"
+
+    def get(self, request):
+        event_form = EventForm()
+        team_form = TeamForm(prefix="team")
+        return render(
+            request,
+            self.template_name,
+            {"event_form": event_form, "team_form": team_form},
+        )
+
+    def post(self, request):
+        event_form = EventForm(request.POST)
+        team_form = TeamForm(request.POST, prefix="team")
+
+        team_has_data = team_form.has_changed()
+
+        if not event_form.is_valid() or (team_has_data and not team_form.is_valid()):
+            return render(
+                request,
+                self.template_name,
+                {"event_form": event_form, "team_form": team_form},
+            )
+
+        event = event_form.save()
+
+        if team_has_data:
+            team = team_form.save(commit=False)
+            team.event = event
+            team.save()
+
+        return redirect("events:event_detail", pk=event.pk)
 
 
 class EventWizard(SessionWizardView):
