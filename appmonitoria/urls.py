6f98a8039b01e34d6d425d53a543f5eafacc392d
Denny Erikson <denny.erikson@gmail.com>
2025-11-27 17:28:29 -0300

    feat: add login and logout functionality
    - Implemented login view
    - Implemented logout view
    - Added login template
    - Added navbar login/logout

--- appmonitoria/urls.py
+++ appmonitoria/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from users.views import UserViewSet, ProfileViewSet, RoleViewSet
@@ -57,6 +58,8 @@ router.register(r'resorts', ResortViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("login/", auth_views.LoginView.as_view(template_name="auth/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
     path('api/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
